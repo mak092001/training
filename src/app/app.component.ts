a9e894304c6fac1ba291137b2183ad85b4bfa8eb
mak <private.mak09@gmail.com>
2022-02-08 19:34:11 +0530

    mak training

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,6 @@
 import { Component } from '@angular/core';
-
 @Component({
-  selector: 'app-root',
+  selector: 'app-component',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'training';
-}
+export class AppComponent {}
---
+++ tailwind.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  theme: {
+    extend: {},
+  },
+  content: ["./src/**/*.{html,js}"],
+  plugins: [],
+};
