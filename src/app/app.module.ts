5c7952e533e38247bd3109fd28a4758616d1a2ff
mak <private.mak09@gmail.com>
2022-02-10 19:00:18 +0530

    mak 7:00

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,9 +4,10 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
+import { Card1Component } from './card1/card1.component';
 
 @NgModule({
-  declarations: [AppComponent, HeaderComponent, FooterComponent],
+  declarations: [AppComponent, HeaderComponent, FooterComponent, Card1Component],
   imports: [BrowserModule],
   providers: [],
   bootstrap: [AppComponent],
